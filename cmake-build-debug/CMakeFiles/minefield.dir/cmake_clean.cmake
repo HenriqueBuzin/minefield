1104a70b30e56f97357a97e3db6351165b654f9f
Henrique Buzin <cultura.henrique@gmail.com>
2020-05-05 00:30:25 -0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/minefield.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minefield.dir/main.cpp.o"
+  "minefield"
+  "minefield.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minefield.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
